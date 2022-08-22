b3bd0eb15175958eed69dc1667cdeee7af89ad9f
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-08-22 20:17:37 +0000

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write('and that piece of art is useful - Dora Kopra, 2015-10-19\n')
+sys.stderr.write('and that piece of art is useful - Dora Kopra, 2015-10-19\n')
 sys.exit(1)
