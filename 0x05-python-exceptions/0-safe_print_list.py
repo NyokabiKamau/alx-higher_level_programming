f34bd7451c31ab5bfbf70fd5762c0709d824ab9e
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-09-12 16:40:19 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def safe_print_lists(my_list=[], x=0):
+    count = 0
+    for i in range(0, x):
+        try:
+            print(f'{my_list[i]}', end="")
+            count += 1
+        except:
+            pass
+    print()
+    return count
