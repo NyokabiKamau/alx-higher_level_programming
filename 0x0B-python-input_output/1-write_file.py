31408007c14598aeb997830e2c2c5fa02c3918cc
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-09-27 16:55:56 +0000

    1-main.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,15 @@
-#/!usr/bin/python3
-"""function that writes a string to a text file (UTF8)
-and returns the number of characters written
-"""
+#!/usr/bin/python3
+"""function that writes a string to a text file(UTF8) and
+    returns the number of chars written"""
 
 
 def write_file(filename="", text=""):
-    """You must use the with statement
-    You don’t need to manage file permission exceptions.
-    Your function should create the file if doesn’t exist.
-    Your function should overwrite the content of the file if it already exists
+    """uses open() and write() to write string to a text file.
+    Return:
+        no of characters read
+    Args:
+        filename: text file to write into
+        text (str): string to write to text file
     """
-
     with open(filename, 'w', encoding='utf8') as file:
         return file.write(text)
